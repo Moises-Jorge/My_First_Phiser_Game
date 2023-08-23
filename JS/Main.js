1beb2c18c0c7b6e239ef463fab99d0ad0fb8431c
Moisés Jorge <moisesjorge30@gmail.com>
2023-08-23 08:54:48 +0100

    I added the personage at the scene of the game
    In this moment, i added the personage of the game at the game's scene, and i defined the physical characteristics of the game using the gravity. This configurations, i did in the our config variabel (at the Main.js file), and i applied in the our Scene01.js file

--- JS/Main.js
+++ JS/Main.js
@@ -6,7 +6,14 @@ window.onload = function() {
         type: Phaser.AUTO, // Definição do tipo de tecnologia que vai ser usado para renderizar o jogo. Também poderíamos usar: Phaser.Canvas || Phaser.WebGL
         width: 800, // Definição da largura e altura da tela do jogo
         height: 600,
-        scene: [Scene01]
+        scene: [Scene01],
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: {y: 1000}
+            }
+        },
+        pixelArt: true
     }
 
     game = new Phaser.Game(config) // esta var foi declarada de forma global, mas foi refenciada aqui dentro da função.
--- JS/Scene01.js
+++ JS/Scene01.js
@@ -6,6 +6,7 @@ class Scene01 extends Phaser.Scene {
     // Método para o carregamento dos recursos iniciais do jogo.
     preload() {
         this.load.image('sky', 'img/sky.png')
+        this.load.spritesheet('player', 'img/player.png', {frameWidth: 32, frameHeight: 32}) // spritesheet pq está a se carregar um ficheiro com várias imagens, onde cada uma delas representa uma posição diferente do personagem.
     }
 
     // Método para a criação e configuração inicial desses recursos no jogo
@@ -13,6 +14,11 @@ class Scene01 extends Phaser.Scene {
         this.sky = this.add.image(0, 0, 'sky').setOrigin(0, 0)
         this.sky.displayWidth = 800
         this.sky.displayHeight = 600
+
+        this.player = this.physics.add.sprite(50, 500, 'player')
+        .setCollideWorldBounds(true)
+        .setScale(2, 2) // Alterando o tamanho do persogem. Por ser um método que pode ser encadeado, então pode se usar deste jeito. É a mesma coisa que fazer: "this.player = algo.func1().func2().funcN"
+        
     }
 
     // Método de Actualização, onde vai se estabelecer as dinâmicas e regras do jogo.
